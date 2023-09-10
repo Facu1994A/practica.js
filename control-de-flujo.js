653d928eb027a83c96b1dedc9dadc951600ddefd
facundo <facundo2027@hotmail.com>
2023-09-10 00:46:56 -0300

    se creo un nuevo repositorio para practicar js

---
+++ control-de-flujo.js
@@ -0,0 +1,97 @@
+/* var nombre = "juan"; */
+
+/* let nombre = "juan";
+
+function saludar() {
+    console.log("Hola" + nombre)
+}
+
+saludar() */
+
+/* function saludar() {
+    let nombre = "juan"
+
+    console.log("Hola" + nombre)
+}
+
+saludar()
+
+console.log(nombre); */
+
+/* if (true) {
+    var nombre = "juan"
+    console.log("Hola" + nombre)
+
+}
+
+console.log(nombre); */
+
+/* let = a = 1, b = "2"; */
+
+/* Operadores de comparacion
+a == b si contienen el mismo valor - no evalua el tipo de dato
+
+a === b se evalua tanto tipo de dato como valor en si. */
+
+/* a != b se evalua si tienen distinto valor
+a !== b se evalua si son distinto valor y tipo */
+
+
+/* if (a !== b) {
+    console.log("Es cierto que son distintos");
+}
+else {
+    console.log("No es cierto que son distintos");
+} */
+
+/* let dia = 4;
+
+if (dia === 1) {
+    console.log("lunes")
+} else if (dia === 2) {
+    console.log("martes")
+} else if (dia === 3) {
+    console.log("miercoles")
+} else if (dia === 4) {
+    console.log("jueves")
+} else if (dia === 5) {
+    console.log("viernes")
+} else if (dia === 6) {
+    console.log("sabado")
+} else if (dia === 7) {
+    console.log("domingo")
+} else {
+    console.log("Valor ingresado incorrecto")
+} */
+
+/* let dia = 4;
+console.log(dia)
+switch (dia) {
+    case 1:
+        console.log("Lunes")
+        break;
+    case 2:
+        console.log("Martes")
+        break;
+    case 3:
+        console.log("Miercoles")
+        break;
+    case 4:
+        console.log("Jueves")
+        break;
+    case 5:
+        console.log("Viernes")
+        break;
+    case 6:
+        console.log("Sabado")
+        break;
+    case 7:
+        console.log("Domingo")
+        break;
+
+    default:
+        console.log("valor incorrecto")
+        break;
+} */
+
+
